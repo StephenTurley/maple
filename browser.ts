3564efd28d38d2f5489b6d56622aa4235aca7972
Stephen Turley <stephen@turley.dev>
2021-01-27 21:49:38 -0500

    added parcel and got divs rendering

--- browser.ts
+++ browser.ts
@@ -3,15 +3,22 @@ import render from './render'
 
 type Sandbox<model, msg> = {
   init: () => model
-  update: (model, msg) => model
-  view: (model) => Html
+  update: (model: model, msg: msg) => model
+  view: (model: model) => Html
 }
-declare var Maple: {
-  init: (HTMLElement) => void
+
+interface Maple {
+  init: (root: HTMLElement) => void
+}
+
+declare global {
+  interface Window {
+    Maple: Maple
+  }
 }
 
 function sandbox<model, msg>(sandbox: Sandbox<model, msg>) {
-  Maple = {
+  window.Maple = {
     init: (root: HTMLElement) => {
       const model = sandbox.init()
       render(root, sandbox.view(model))
